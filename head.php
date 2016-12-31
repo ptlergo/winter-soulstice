21c48a1cf78c11df40e9b55c012dc39c0b11565b
ptlergo <ptlergo@gmail.com>
2016-12-31 11:20:14 -0500

    metatags update

--- head.php
+++ head.php
@@ -1,11 +1,13 @@
 	  <meta charset="utf-8"/>
-	  <meta name="title" content="Winter Soulstice Concert Exclusive RSVPs"/>
+		<meta name="title" content="Winter Soulstice Concert Exclusive RSVPs"/>
+		<meta name="author" content="Patrick Funom Tunga-Lergo"/>
+		<meta name="keywords" content="Patrick, Funom, Tunga-Lergo, php, backbooth, dothello, .hello{}, 8pounds, concert">
 	  <meta name="description" content="App. for limited VIP tickets reservations for Winter Soulstice concert at BackBooth."/>
 	  <meta/>
 	  <meta/>
 	  <meta name="viewport" content="width=device-width, initial-scale=1"/>
-	  <link rel="image_src" href="../images/ws-favicon.png" />
+	  <link rel="image_src" href="/images/ws-favicon.png" />
 	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
-	  <link rel="stylesheet" type="text/css" href="../public/css/main.css"/>
+	  <link rel="stylesheet" type="text/css" href="/public/css/main.css"/>
 	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
