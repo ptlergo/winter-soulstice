de5a68360ea054602969c4ca40438ea0333dde63
ptlergo <ptlergo@gmail.com>
2016-12-31 11:27:58 -0500

    readme update

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-# winter soulstice
-Event RSVP application built with PHP/CodeIgniter
+# Winter Soulstice Concert RSVP App.
+RSVP application built with PHP/CodeIgniter.
+Used for a January 2016 concert at BackBooth Lounge, Orlando, FL
 
+# Install
+clone the repo
+```
+git clone https://github.com/ptlergo/winter-soulstice.git
+```
+
+
+# Run
+running your local apache server visit the project's location. EX:
+```
+http://localhost:8080/winter-soulstice/index.php
+```
 ## Ported over from c9 project
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!---
 	Editor: Patrick Tunga-Lergo
-	A .Hello{} Website
+	App Powered by: .Hello{}
 	Wintersoulstice
 -->
 
@@ -24,34 +24,34 @@
 				<p class="col-xs-12" style="text-align: center; margin-bottom: 15%; color: #ffffff; font-style: italic;">
 					<a href="https://www.facebook.com/events/999514656756811/">RSVP for January 5th concert, 8PM at BackBooth</a>
 				</p>
-				
+
 				<form role="form" class="form-horizontal" action="connect.php" enctype="multipart/form-data" method="POST">
 						<div class="col-xs-10 col-xs-offset-1 ">
 							<input class="form-control input-lg" type="text" name="name" placeholder="enter your full name" required />
 						</div>
-		
+
 						<div class="col-xs-10 col-xs-offset-1">
 							<input class="form-control input-lg" type="email" name="email" placeholder="enter your email" required/>
 						</div>
-			
+
 						<!--<div class="col-xs-10 col-xs-offset-1">-->
 						<!--	<input class="form-control input-lg" type="phone" name="phone" placeholder="10 digit phone number"/>-->
 						<!--</div>-->
-						
+
 						<div class="col-xs-10 col-xs-offset-1 col-md-12 tickets-select" style="text-align: center">
 							<label for="tickets"><p style="color: #fed136" >how many tickets will you reserve?</p></label>
-						
+
 							<div class="clearfix"></div>
-							
+
 							<label class="radio-inline"><input type="radio" name="tickets" value="1" required >1</label>
 							<label class="radio-inline"><input type="radio" name="tickets" value="2" checked>2</label>
 							<label class="radio-inline"><input type="radio" name="tickets" value="3">3</label>
 							<label class="radio-inline"><input type="radio" name="tickets" value="4">4</label>
 							<label class="radio-inline"><input type="radio" name="tickets" value="5">5</label>
 							<label class="radio-inline"><input type="radio" name="tickets" value="6">6</label>
-	
+
 						</div>
-	
+
 						<div class="col-xs-12 col-sm-12 submit">
 							<input id="submit" name="submit" type="submit" value="SUBMIT" class="btn btn-primary col-xs-12" />
 						</div>
