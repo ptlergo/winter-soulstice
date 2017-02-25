30443c507463b60365fc7647dcbcfc4aeee7fa6b
ptlergo <ptlergo@gmail.com>
2017-02-25 17:59:46 -0500

    readme update

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Winter Soulstice Concert RSVP App.
 ###My first PHP based mobile app.
+
+# Details
+Mobile first design styled with bootstrap using codeigniter php framework.
 Used for a January 2016 concert at BackBooth Lounge, Orlando, FL.
 
 # Install
@@ -13,9 +16,6 @@ running your local apache server visit the project's location. EX:
 ```
 http://localhost:8080/winter-soulstice/index.php
 ```
-
-# Details
-styled with bootstrap. Best viewed on mobile devices.
 
 ## Ported over from Cloud9 [project](https://wintersoulstice-ptlergo.c9users.io/)
 visit live project here -> [https://winter-soulstice.herokuapp.com/](https://winter-soulstice.herokuapp.com/)
--- navbar.php
+++ navbar.php
@@ -1,23 +1,9 @@
 		<nav class="navbar navbar-default">
 		  <div class="container-fluid">
 		    <div class="navbar-header" >
-        <!--   <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigationbar">-->
-		      <!--  <span class="sr-only">Toggle navigation</span>-->
-		      <!--  <span class="icon-bar"></span>-->
-		      <!--  <span class="icon-bar"></span>-->
-		      <!--  <span class="icon-bar"></span>-->
-		      <!--</button>-->
 		      <a class="navbar-brand" href="index.php">RSVP</a>
 		      <a class="navbar-brand" href="artistspage.php">Artists</a>
 		      <a class="navbar-brand" href="about.php">About</a>
 		    </div>
-		
-		    <!--<div class="collapse navbar-collapse" id="navigationbar">-->
-		    <!--  <ul class="nav navbar-nav">-->
-		    <!--    <li class="active"><a href="artistspage.php">Artists <span class="sr-only">(current)</span></a></li>-->
-		    <!--    <li><a href="about.php">About/Contact</a></li>-->
-		    <!--    </li>-->
-		    <!--  </ul>-->
-		    <!--</div>-->
 		  </div>
 		</nav>
