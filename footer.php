68d30c528706b19d83a8049e85d51b6e3a0cdec9
ptlergo <ptlergo@gmail.com>
2017-02-25 17:48:42 -0500

    logo change for 8lb

--- footer.php
+++ footer.php
@@ -1,17 +1,17 @@
 			<div class="footer">
 				<div class="col-xs-12 col-md-12 f-content">
-				
+
 					<div class="col-xs-10 col-xs-offset-1" style="text-align: center; font-size: 1.1em;">
 						<a href="http://backbooth.com/about.php"><img src="images/bbooth.png" alt="bbooth-logo"></img></a>
 						<br>
 						<a href="https://www.google.com/maps/place/37+W+Pine+St,+Orlando,+FL+32801/@28.5412822,-81.3831335,17z/data=!3m1!4b1!4m2!3m1!1s0x88e77affb29cf695:0x3f4b8030377cb104?hl=en">
 							            37 West Pine Street, Orlando, FL 32801</a>
 					</div>
-					
+
 					<div class="col-xs-12 col-sm-12 col-md-12" style="text-align: center; margin-top: 3%; margin-bottom:0; padding-bottom:0; ">
 						<p style="font-style: italic">powered by<p>
 						<a href="#"><img src="images/hello-logo.png" alt="hello-logo"></img></a>&nbsp;&nbsp;&nbsp;
-						<a href="https://www.instagram.com/eightpounds/"><img src="images/8lb-avatar.png" alt="hello-logo" width="10%"></img></a>
+						<a href="https://www.instagram.com/eightpounds/"><img src="images/8lb-avatar.svg" alt="hello-logo" width="10%"></img></a>
 					</div>
 				</div>
 			</div>
